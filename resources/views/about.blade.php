3dc56000cdd1bb5461c423d6a35e1913dada74e5
zacktagnan <zacktagnan@gmail.com>
2026-03-13 23:55:14 +0100

    feat: add about page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acerca de - Git for Pro</title>
+</head>
+<body>
+    <h1>Acerca de</h1>
+    <p>Este proyecto fue implementado con Laravel y Sail, entre otras cosas.</p>
+</body>
+</html>
