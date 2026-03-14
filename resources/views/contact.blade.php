45a1dbe5c57b189464927918fcf7be0855d0de39
zacktagnan <zacktagnan@gmail.com>
2026-03-14 15:07:08 +0100

    feat: add contact page

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacto - Git for Pro</title>
+</head>
+<body>
+    <h1>Contacto</h1>
+    <p>Enviar consultas a <a href="mailto:aloha@gitforpro.es" title="Enviar email">aloha@gitforpro.es</a></p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get("/", function () {
 Route::get("/about", function () {
     return view("about");
 });
+
+Route::get("/contact", function () {
+    return view("contact");
+});
