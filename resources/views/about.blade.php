e2fc350e9f4e73dde61979ed8848d963eb5d1638
zacktagnan <zacktagnan@gmail.com>
2026-03-16 00:29:33 +0100

    docs: add tagline to about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,7 +5,10 @@
 <x-layouts.app :title="$title">
     <h1 class="text-2xl font-bold">{{ $title }}</h1>
 
-    <p class="mt-4">Este proyecto fue implementado con Laravel (v.12.54.1) y Sail, entre otras cosas.</p>
+    <p class="mt-4">Este proyecto fue implementado con Laravel (v.12.54.1) y Sail, entre otras cosas.
+        <br><small class="ml-4 px-1 pt-0 pb-0.5 rounded bg-gray-400 text-white">Licencia MIT</small></p>
 
-    <p class="mt-4"><small>Licencia MIT.</small></p>
+    <p class="mt-4"></p>
+
+    <p class="mt-4 italic text-sm">"Desarrollando con cariño..." <span class="text-sky-400 font-bold">:)</span></p>
 </x-layouts.app>
