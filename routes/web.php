59ccd0ea289505a0e3a62db9bd189a08f77b2aa7
zacktagnan <zacktagnan@gmail.com>
2026-03-16 22:52:58 +0100

    refactor: restructuring routes format

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get("/", function () {
-    return view("home");
-});
-
-Route::get("/services", function () {
-    return view("services");
-});
-
-Route::get("/about", function () {
-    return view("about");
-});
-
-Route::get("/contact", function () {
-    return view("contact");
-});
-
 // = private documentation... =================================
-Route::get("/docs", function () {
-    return view("docs.index");
-});
+// Route::get("/docs", function () {
+//     return view("docs.index");
+// });
+// o
+// Route::get("/docs", fn() => view("docs.index"));
+// o
+Route::view("/docs", "docs.index");
 // = private documentation... =================================
+
+Route::view("/", "home");
+
+Route::view("/services", "services");
+
+Route::view("/about", "about");
+
+Route::view("/contact", "contact");
