4726a0257052c72169733b40effa0778261862e2
zacktagnan <zacktagnan@gmail.com>
2026-03-18 02:45:31 +0100

    feat: improve navigation with all page links

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,5 +1,7 @@
     <nav class="*:rounded-full *:border *:border-sky-100 *:bg-sky-50 *:hover:bg-sky-200 *:px-2 *:py-0.5 dark:text-sky-300 dark:*:border-sky-500/15 dark:*:bg-sky-500/10">
         <a href="/" title="Ir a `Inicio`">Inicio</a>
         <a href="/services" title="Ir a `Servicios`">Servicios</a>
+        <a href="/about" title="Ir a `Sobre Nosotros`" class="pl-5">Sobre Nosotros</a>
+        <a href="/faq" title="Ir a `FAQ`">FAQ</a>
         <a href="/contact" title="Ir a `Contacto`">Contacto</a>
     </nav>
