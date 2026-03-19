3c34a69cc54ad5a2a3c46e522582b001e61bbe80
PJ-CM <zacktagnan@gmail.com>
2026-03-19 20:42:42 +0100

    refactor: update contact email paragraph for clarity
    (causing commit conflicts)

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,5 +5,5 @@
 <x-layouts.app :title="$title">
     <h1 class="text-2xl font-bold">{{ $title }}</h1>
 
-    <p class="mt-4">Enviar consultas a <a href="mailto:aloha@gitforpro.es" class="text-blue-500 hover:underline" title="Enviar email">aloha@gitforpro.es</a> siempre que sea necesario.</p>
+    <p class="mt-4">Enviar consultas a <a href="mailto:aloha@gitforpro.es" class="text-blue-500 hover:underline" title="Enviar email">aloha@gitforpro.es</a> para resolver cualquier duda.</p>
 </x-layouts.app>
